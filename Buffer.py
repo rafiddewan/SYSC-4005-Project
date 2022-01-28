37a2e80a7b268702351b95bc8b33574dd3f18ace
Nick Coutts <Nickcoutts99@gmail.com>
2022-01-28 16:47:35 -0500

    Added boilerplate code for classes

---
+++ Buffer.py
@@ -0,0 +1,26 @@
+from Workstation import WorkStation
+
+
+class Buffer:
+
+    def __init__(self, workstation: WorkStation):
+        self.size = 0
+        self.workstation = workstation
+    
+    def getSize(self):
+        return self.size
+    
+    def addComponent(self):
+        self.size +=1
+    
+    def removeComponent(self):
+        self.size -=1
+    
+    def isFull(self):
+        return self.size == 2
+
+    def setWorkstation(self, workstation: WorkStation):
+        self.workstation = workstation
+
+    def getWorkstation(self):
+        return self.workstation
---
+++ Workstation.py
@@ -0,0 +1,24 @@
+from os import access
+
+
+class WorkStation:
+    
+    def __init__(self):
+        self.active = False
+        self.timeRemaining = 0
+
+    def getActive(self) -> bool:
+        return self.active
+
+    def setActive(self, active: bool) -> None:
+        self.active = active
+    
+    def toggleActive(self):
+        self.active = not self.active
+
+    def getTimeRemaining(self) -> int:
+        return self.timeRemaining
+
+    def setTimeRemaining(self, timeRemaining: int) -> None:
+        self.timeRemaining = timeRemaining
+    
